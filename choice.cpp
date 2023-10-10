6e1984589bddcba12fbba0e5c6644458b12384fe
Bruce <147499088+BruceWayne1963@users.noreply.github.com>
2023-10-10 15:41:27 +0530

    Create choice.cpp

---
+++ choice.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+int c;
+
+cout<<"You either die a hero, or live long enough to see yourself become the villain. The choice is yours";
+cout<<"So, what is it gonna be? 1 or 2:";
+cin>>c;
+if(c == 1)
+{
+cout<<"Congratulations hero";
+}
+else if(c == 2){
+cout<<"The Joker would like to have a word with you";
+}
+//rlrzpmgemt
+return 0;
+}
